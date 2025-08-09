a9074425391db4d0f09b4005be9df130f5483286
dingdinglz <1149978856@qq.com>
2025-08-09 14:47:26 +0800

    🐳 chore(config): 更新vite配置和Node.js版本

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,21 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.NODE_ENV === 'production' ? '/tic-index-2/' : '/',
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vue: ['vue'],
+          gsap: ['gsap'],
+          glightbox: ['glightbox']
+        }
+      }
+    }
+  },
+  define: {
+    global: 'globalThis',
+  }
 })
